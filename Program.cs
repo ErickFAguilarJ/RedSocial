0e7f64502d3da3a5ca4664aec5624f77bdebe508
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-11-09 13:44:29 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedSocial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
